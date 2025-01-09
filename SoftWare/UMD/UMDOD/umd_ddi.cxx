706a20151c32b0c8d77f3193933d75705d071d4d
liguoqi <779163410@qq.com>
2025-01-09 22:05:55 +0800

    re build prj

---
+++ SoftWare/UMD/UMDOD/umd_ddi.cxx
@@ -0,0 +1,18 @@
+/***************************************************************\
+	Author : Guoqi Li
+	Date   : 2025/1/9
+	Funcation: User mode DDI
+\***************************************************************/
+
+#include<Process.h>
+#include<minwindef.h>
+
+
+
+BOOL WINAPI DllMain(
+	HINSTANCE hinstDLL,  // handle to DLL module
+	DWORD fdwReason,     // reason for calling function
+	LPVOID lpvReserved)  // reserved
+{
+
+}
